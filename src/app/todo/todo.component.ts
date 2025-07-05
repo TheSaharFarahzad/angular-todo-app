5277449fe11928603ec2219b39d9f9e8813d4c0b
Sahar <shr.farahzad@gmail.com>
2025-07-05 22:39:25 +0330

    feat: implement todo with add, delete and toggle functionality

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Todo } from './todo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.css'
+  styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   name = new FormControl('');
+  todos: Todo[] = [];
+
+  ngOnInit() {
+    const saved = localStorage.getItem('todos');
+    if (saved) {
+      this.todos = JSON.parse(saved);
+    }
+  }
+
+  addTodo() {
+    const value = this.name.value?.trim();
+    if (!value) return;
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: value,
+      completed: false
+    };
+
+    this.todos.push(newTodo);
+    this.name.setValue('');
+    this.saveTodos();
+  }
+
+  toggleTodo(id: number) {
+    const todo = this.todos.find(t => t.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+      this.saveTodos();
+    }
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter(t => t.id !== id);
+    this.saveTodos();
+  }
+
+  saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 }
