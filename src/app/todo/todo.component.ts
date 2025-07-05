63752d1850a16da9db87f57031a13f314bc991b1
Sahar <shr.farahzad@gmail.com>
2025-07-05 11:35:17 +0330

    feat: add todo component with reactive form and integrate it into app component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
-
+  name = new FormControl('');
 }
